393e34ba3e4162637b1b8c0d33eb967ba464eece
karo <karolin.kostial@gmail.com>
2025-04-18 20:42:38 +0200

    Add `create` method to ClientRepository trait
    Introduced a `create` method in the `ClientRepository` trait to allow adding new clients using the `CreateClientParams` struct. Also added a placeholder implementation for this method in the `InMemoryClientRepository`.

--- src/repository/client_repository.rs
+++ src/repository/client_repository.rs
@@ -3,10 +3,16 @@ use std::sync::Arc;
 use async_trait::async_trait;
 use shaku::{Component, Interface};
 use tokio::sync::RwLock;
-use crate::domain::client::Client;
+use crate::domain::client::{Client, ClientId};
+
+pub struct CreateClientParams {
+    pub redirect_uris: Vec<String>,
+    pub scopes: Vec<String>,
+}
 
 #[async_trait]
 pub trait ClientRepository: Interface {
+    async fn create(&self, client: CreateClientParams) -> Result<Client, String>;
     async fn find_by_id(&self, id: &str) -> Option<Client>;
 }
 
@@ -27,6 +33,10 @@ impl InMemoryClientRepository {
 
 #[async_trait]
 impl ClientRepository for InMemoryClientRepository {
+    async fn create(&self, params: CreateClientParams) -> Result<Client, String> {
+        unimplemented!()
+    }
+
     async fn find_by_id(&self, id: &str) -> Option<Client> {
         let clients = self.clients.read().await;
         clients.get(id).cloned()
