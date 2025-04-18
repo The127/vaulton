cb1c2dfa67193da3074cc1f673eab0257a039141
karo <karolin.kostial@gmail.com>
2025-04-18 13:34:36 +0200

    Add AppState with OAuthModule to server setup
    Introduced an `AppState` struct containing an `OAuthModule` wrapped in an `Arc` for shared access. This sets up the dependency injection module and integrates it into the server initialization process.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,13 +1,31 @@
 mod health;
 
+use std::sync::Arc;
 use crate::{oidc, Config};
 
 use axum::{Router, routing::get};
+use crate::repository::client_repository::OAuthModule;
+
+#[derive(Clone)]
+pub struct AppState {
+    module: Arc<OAuthModule>,
+}
 
 pub async fn create_server(config: Config) -> Router {
+    // Create the DI module
+    let module = Arc::new(
+        OAuthModule::builder()
+            .build()
+    );
+
+    // Create the app state
+    let state = AppState {
+        module,
+    };
+
     // Create a new router with a single route
     Router::new()
+        .with_state(state)
         .route("/health", get(health::health_check))
         .merge(oidc::oidc_routes(config))
-
 }
