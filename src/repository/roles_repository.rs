10f2b41fe6ea6dd5ca968135c3aebdf3b39f6b23
karo <karolin.kostial@gmail.com>
2025-04-19 10:09:36 +0200

    Add create and find_by_id methods to RoleRepository
    Implemented `create` for inserting new roles into the database and `find_by_id` for retrieving roles by ID. These methods leverage SQLx queries and enhance the repository's functionality for role management.

--- src/repository/roles_repository.rs
+++ src/repository/roles_repository.rs
@@ -12,6 +12,8 @@ pub struct CreateRoleParams {
 
 #[async_trait]
 pub trait RoleRepository: Interface {
+    async fn create(&self, role: CreateRoleParams) -> Result<Role, String>;
+    async fn find_by_id(&self, id: Uuid) -> Option<Role>;
 }
 
 #[derive(Component)]
@@ -29,5 +31,48 @@ impl PostgresRoleRepository {
 
 #[async_trait]
 impl RoleRepository for PostgresRoleRepository {
-    
+    async fn create(&self, params: CreateRoleParams) -> Result<Role, String> {
+        let result = sqlx::query!(
+            r#"
+            insert into roles (name, description)
+            values ($1, $2)
+            returning id as "uuid", name, description, created_at, updated_at
+            "#,
+            params.name.clone(),
+            params.description,
+        )
+        .fetch_one(self.pool.get_pool())
+        .await
+        .map_err(|e| e.to_string())?;
+
+        Ok(Role {
+            uuid: result.uuid,
+            name: result.name,
+            description: result.description,
+            created_at: result.created_at,
+            updated_at: result.updated_at,
+        })
+    }
+
+    async fn find_by_id(&self, id: Uuid) -> Option<Role> {
+        let result = sqlx::query!(
+            r#"
+            select id as "uuid", name, description, created_at, updated_at
+            from roles
+            where id = $1
+            "#,
+            id.clone()
+        )
+        .fetch_optional(self.pool.get_pool())
+        .await
+        .ok()??;
+
+        Some(Role {
+            uuid: result.uuid,
+            name: result.name,
+            description: result.description,
+            created_at: result.created_at,
+            updated_at: result.updated_at,
+        })
+    }
 }
